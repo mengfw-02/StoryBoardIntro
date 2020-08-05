3592c7b56a9abd77b056288fd7e09ae4f0e3dd53
mengfw-02 <63938598+mengfw-02@users.noreply.github.com>
2020-08-05 08:34:04 -0700

    My first XCode project!

---
+++ StoryBoardIntro/StoryBoardIntro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoryBoardIntro
+//
+//  Created by Mengfan Wang on 8/5/20.
+//  Copyright © 2020 Mengfan Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("This is a print statement - testing testing!")
+    }
+
+
+}
+
